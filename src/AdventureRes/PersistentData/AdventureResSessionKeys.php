b8470a9f9e5ebf90fa7ec59df955147bc3433ab9
Ben Wilkins <bwilkins@paramoredigital.com>
2016-05-05 16:22:46 -0500

    Making updates to the session.
    * Abstracting session keys into their own class.
    * Adding a default value to the `get` method of the data handler class.

---
+++ src/AdventureRes/PersistentData/AdventureResSessionKeys.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright 2016 AdventureRes
+ *
+ * @license GPL-3.0+
+ */
+
+namespace AdventureRes\PersistentData;
+
+class AdventureResSessionKeys
+{
+    const SESSION_ID = 'session_id';
+    const RESERVATION_ID = 'reservation_id';
+}
+
+/* End of AdventureResSessionKeys.php */
