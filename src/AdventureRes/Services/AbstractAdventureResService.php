7b19c5e740cd2af8b1766ee666575fe10644fda5
cameron west <cwest@paramoredigital.com>
2017-08-30 15:16:10 -0500

    Adding methods to get a reservation ID or set a reservation ID for any API endpoint handler

--- src/AdventureRes/Services/AbstractAdventureResService.php
+++ src/AdventureRes/Services/AbstractAdventureResService.php
@@ -11,6 +11,7 @@ use AdventureRes\AbstractAdventureResBase;
 use AdventureRes\AdventureResApp;
 use AdventureRes\Http\AdventureResRequest;
 use AdventureRes\Authentication\AdventureResSession;
+use AdventureRes\PersistentData\AdventureResSessionKeys;
 use AdventureRes\HttpClients\AdventureResHttpClientInterface;
 use AdventureRes\AdventureResClient;
 
@@ -67,7 +68,7 @@ class AbstractAdventureResService extends AbstractAdventureResBase
     /**
      * Sets the configuration app.
      *
-*@param \AdventureRes\AdventureResApp $app
+     * @param \AdventureRes\AdventureResApp $app
      */
     public function setApp($app)
     {
@@ -77,7 +78,7 @@ class AbstractAdventureResService extends AbstractAdventureResBase
     /**
      * Sets the HTTP client to be used in requests.
      *
-*@param AdventureResHttpClientInterface $httpClient
+     * @param AdventureResHttpClientInterface $httpClient
      */
     public function setHttpClient($httpClient)
     {
@@ -88,7 +89,7 @@ class AbstractAdventureResService extends AbstractAdventureResBase
      * Gets the session ID to be used in requests. Uses the AdventureResSession class to validate and/or generate
      * a session ID.
      *
-*@return string
+     * @return string
      */
     public function getSessionId()
     {
@@ -104,11 +105,36 @@ class AbstractAdventureResService extends AbstractAdventureResBase
     /**
      * Sets the session ID.
      *
-*@param string $sessionId
+     * @param string $sessionId
      */
     public function setSessionId($sessionId)
     {
         $this->sessionId = $sessionId;
+    }
+
+    /**
+     * Get the reservation ID for our current session
+     *
+     * @return mixed
+     */
+    public function getReservationId()
+    {
+        $dataHandler = $this->app->getDataHandler();
+
+        return $dataHandler->get(AdventureResSessionKeys::RESERVATION_ID);
+    }
+
+    /**
+     * Get the reservation ID for our current session
+     *
+     * @param $reservationId
+     * @return mixed
+     */
+    public function setReservationId($reservationId)
+    {
+        $dataHandler = $this->app->getDataHandler();
+
+        return $dataHandler->set(AdventureResSessionKeys::RESERVATION_ID, $reservationId);
     }
 
     /**
